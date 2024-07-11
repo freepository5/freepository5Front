3939a1770a81881bffe72c2b10025c52d7eb165c
AdiyieR26 <radoine1996@gmail.com>
2024-07-11 08:57:53 +0200

    add codding register.component.html and logic in register.component.ts

--- freepository-project/src/app/auth/register/register.component.ts
+++ freepository-project/src/app/auth/register/register.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  private formBuilder= inject(FormBuilder)
+  router = inject(Router);
+  registerForm!:FormGroup;
 
+  ngOnInit(): void {
+      
+    this.registerForm=this.formBuilder.group({
+      name:["", Validators.required],
+      email:["", [Validators.required, Validators.email]],
+      password:["", [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  onSubmit(){
+    if(this.registerForm.valid){
+
+      const formData = this.registerForm.value;
+
+      name: this.registerForm.controls["name"].value;
+      email:this.registerForm.controls["email"].value;
+      password:this.registerForm.controls["password"].value;
+    } 
+  }
 }
