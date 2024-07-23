5ba0343047e8a75c8c3b060512f865c351e2c82e
chichilahore <alexandrchichiloza@gmail.com>
2024-07-23 23:20:58 +0200

    search architecture changed and refactoring classes in components

--- freepository-project/src/app/components/searcher/searcher.component.ts
+++ freepository-project/src/app/components/searcher/searcher.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-searcher',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './searcher.component.html',
   styleUrl: './searcher.component.scss'
 })
-export class SearcherComponent {
 
+export class SearcherComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    const input = document.getElementById('search-input') as HTMLInputElement;
+    const placeholder = document.querySelector('.placeholder') as HTMLDivElement;
+
+    input.addEventListener('focus', () => {
+      placeholder.style.opacity = '0';
+    });
+
+    input.addEventListener('blur', () => {
+      if (input.value === '') {
+        placeholder.style.opacity = '1';
+      }
+    });
+  }
 }
