f8e2be3580a4b91aaf0eab441e02aa19e5f5e07c
chichilahore <alexandrchichiloza@gmail.com>
2024-07-24 09:49:49 +0200

    changes to header

--- freepository-project/src/app/components/resource/resource.component.ts
+++ freepository-project/src/app/components/resource/resource.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../layout/header/header.component';
 
 @Component({
   selector: 'app-resource',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './resource.component.html',
   styleUrl: './resource.component.scss'
 })
